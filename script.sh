c50c7a16ae54980e4f140134e5415ecbf326041a
YouruiGuo <yourui@ualberta.ca>
2020-11-21 15:31:27 -0700

    script added

---
+++ script.sh
@@ -0,0 +1,20 @@
+if which brew > /dev/null; then
+  # The package is installed
+  echo "brew installed"
+  brew update
+else
+  # The package is not installed
+  echo "brew not installed"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+if which node > /dev/null
+    then
+        echo "node is installed, skipping..."
+    else
+        # add deb.nodesource repo commands
+        # install node
+        echo "not installed"
+        brew install node
+    fi
+npm -g install chromedriver
