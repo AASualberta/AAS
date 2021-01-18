0a479cafbd18d1f4bb8f773975fae097f7a3d5d6
YouruiGuo <yourui@ualberta.ca>
2021-01-18 15:43:12 -0700

    script done

--- face_recog.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-d = {"rmssd": 123} # Root Mean Square of the Successive Differences
-
-r = requests.post('http://127.0.0.1:3000/test', data=d)
-print(r)
-
--- script.sh
+++ script.sh
@@ -10,3 +10,20 @@ if which node > /dev/null
     fi
 npm install
 npm -g install chromedriver
+
+if which python3 > /dev/null
+	then
+		echo "python3 is installed, skipping..."
+	else
+		echo "python3 is not installed"
+		if xcode-select --install > /dev/null
+		then
+			echo "command line tools not installed"
+			/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+			brew install python3
+		else
+			echo "command line tools installed"
+	fi
+fi
+pip3 install requests
+pip3 install opencv-python
